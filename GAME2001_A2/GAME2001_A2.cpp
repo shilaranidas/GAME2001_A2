277d097f4085d7f1e73101bbaff081aa69ed296c
shilaranidas <shilarani.das@georgebrown.ca>
2021-11-28 20:09:17 -0600

    add week 9 lab

--- GAME2001_A2/GAME2001_A2.cpp
+++ GAME2001_A2/GAME2001_A2.cpp
@@ -3,10 +3,10 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
